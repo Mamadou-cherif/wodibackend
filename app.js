bb53d5aa4f1d4eb840989d4d070f3ce771bc7a7d
cherif barry <barrymamadoucherif289@gmail.com>
2021-12-02 17:55:48 +0000

    nouvea

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var vote_routes = require('./routes/vote')
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://wodify.netlify.app/');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 
--- routes/message.js
+++ routes/message.js
@@ -12,7 +12,7 @@ var md_upload = multipart({uploadDir: './uploads/publications/'});
     api.post("/initialize", md_auth.ensureAuth,MessageController.inititChallenge)
     api.put("/update/:id", md_auth.ensureAuth, MessageController.edition)
     api.post('/compt1/:id',MessageController.addCompt1);
-    api.post('/compt2/:id',MessageController.addCompt2);
+    api.post('/compt2/:id',[md_auth.ensureAuth, md_upload],MessageController.addCompt2);
     // api.post('/upload-image-pub/:id',[md_auth.ensureAuth, md_upload],  MessageController.uploadImage);
     api.post('/downoladImage/:id', [md_auth.ensureAuth, md_upload], MessageController.downloadImage)
     api.get('/get-image-pub/:imageFile', MessageController.getImageFile);
